012b7c377b2bea80b5ce1491cea673d18fddf589
Joël LAMOTTE <jlamotte@softbankrobotics.com>
2019-01-29 16:26:44 +0100

    atghrthggfrhrt

--- plop/plop.cxx
+++ plop/plop.cxx
@@ -10,7 +10,9 @@ namespace plop
   void say_hello (ostream& o, const string& n)
   {
     if (n.empty ())
+    {
       throw invalid_argument ("empty name");
+    }
 
     o << "Hello, " << n << "! KIKOO LOL" << endl;
     o << "ROHOLOLOLOL";
